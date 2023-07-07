9860ee36b0092432104a5f90f7c942a9e90c134f
Sergei Popov <sergeipo@playtika.com>
2023-07-07 16:12:06 +0200

    rename

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using test1;
+using test;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
